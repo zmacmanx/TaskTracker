82a0ebb8d0360fd7bd611caa7fcec5dad9262ecc
zmacmanx <zmacmanx@gmail.com>
2019-01-16 14:16:36 -0500

    Adding the baseline module layer and myhome starting point

---
+++ src/app/modules/my-home/my-home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyHomeRoutingModule } from './my-home-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyHomeRoutingModule
+  ]
+})
+export class MyHomeModule { }
