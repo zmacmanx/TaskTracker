e2572b13577657b1452b8bb45e43b18a5b05e65e
zmacmanx <zmacmanx@gmail.com>
2019-01-16 15:00:23 -0500

    Adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SHI TaskTracker';
+  currentYear: number = (new Date()).getFullYear();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyHomeModule } from './modules/my-home/my-home.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MyHomeModule } from './modules/my-home/my-home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MyHomeModule
+    MyHomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
